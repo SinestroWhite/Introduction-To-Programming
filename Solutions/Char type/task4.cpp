62395e14cdb52e74623142606a34df7f22492557
Martin Iliev <mapmoeu@gmail.com>
2020-10-17 20:06:41 +0300

    Add solution to tasks 4

---
+++ Solutions/Char type/task4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+	const char BEG = 'A', END = 'Z', MID = (BEG + END) / 2;
+	char input, symmetric, distance;
+	std::cin >> input;
+
+	// Solution 1
+	distance = (input <= MID ? input - BEG : END - input);
+	symmetric = (input <= MID ? END - distance : BEG + distance);
+	std::cout << symmetric;
+	
+	// Solution 2
+	if (input <= MID) {
+		distance = input - BEG;
+		symmetric = END - distance;
+	} else {
+		distance = END - input;
+		symmetric = BEG + distance; 
+	}
+	std::cout << symmetric;
+
+	// Solution 3
+	symmetric = BEG + END - input;
+	std::cout << symmetric << "\n";
+}
