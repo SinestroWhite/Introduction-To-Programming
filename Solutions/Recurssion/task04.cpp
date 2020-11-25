9c426f11ab377bdb187a8192765aaf1a255c72a5
mapmo <mapmoeu@gmail.com>
2020-11-25 10:27:45 +0200

    Add solutions to some recurssion tasks

---
+++ Solutions/Recurssion/task04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int pow(const int x, const int n) {
+	if (n == 0) {
+		return 1;
+	}
+
+	if (n > 0) {
+		return x * pow(x, n - 1);
+	}
+
+	return x * 1 / pow(x, n + 1);
+}
+
+int main() {
+	std::cout << pow(5, 3);
+
+	return 0;
+}
