b860d57d27452d80fc83fad486af2dd4ef2d757d
mapmo <mapmoeu@gmail.com>
2020-12-11 19:21:40 +0200

    Add solution to task 9 from the strings tasks

---
+++ Solutions/Strings/task09.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+//Напишете програма, която въвежда знаков низ S и като резултат отпечатва онези малки латински букви, които не се съдържат в низа.
+
+#define all 26
+
+int main() {
+	std::string sample;
+	std::cin >> sample;
+
+	bool letter[all] = {};
+
+	for (int i = 0; i < sample.size(); i++) {
+		letter[sample[i] - 'a'] = 1;
+	}
+
+	for (int i = 0; i < all; i++) {
+		if (letter[i] == 0) {
+			std::cout << char(i + 'a') << '\n';
+		}
+	}
+}
