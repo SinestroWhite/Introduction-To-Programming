00c65f7c2bd681363c05b8f600fdac8546129ab3
mapmo <mapmoeu@gmail.com>
2020-12-27 20:50:58 +0200

    Add solution to Homework 3 2020, task 4

---
+++ Solutions/Past Homeworks/2020/3/task04.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#define letters 26
+
+struct trie {
+	trie * children[letters] = {};
+};
+
+void put(trie * solution, const std::string & input, int & counter) {
+	if (input.size() == 0) {
+		return;
+	}
+
+	int first_letter_id = input[0] - 'a';
+	if (solution -> children[first_letter_id] == nullptr) {
+		counter++;
+		solution -> children[first_letter_id] = new trie;
+	}
+
+	std::string tmp = input;
+	tmp.erase(tmp.begin());
+	put(solution -> children[first_letter_id], tmp, counter); 
+}
+
+void free(trie * solution) {
+	for (int i = 0; i < letters; i++) {
+		if (solution -> children[i] != nullptr) {
+			free(solution -> children[i]);
+			delete solution -> children[i];			
+		}
+	}
+}
+
+void split(trie* const solution, const std::string& input, int & counter) {
+	put(solution, input, counter);
+	for (int i = 0; i < input.size(); i++) {
+		std::string tmp = input;
+		tmp.erase(tmp.begin() + i);
+		split(solution, tmp, counter);
+	}
+}
+
+int main() {
+	int counter = 0;
+	trie * solution = new trie;
+	std::string input;
+	std::cin >> input;
+
+	split(solution, input, counter);
+	free(solution);
+
+	std::cout << counter << "\n";
+	return 0;
+}
