7c9e462f7314077d70ae5ae1222626c34f8e4d62
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-10-06 18:31:48 +0300

    Refactor Operators And Ranges...
    Remove task03 (has loops in it)
    Rename task02 to task03
    Make a new task02 for sorting 3 numbers

--- Solutions/Operators And Ranges/task02.cpp
+++ Solutions/Operators And Ranges/task02.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 
 int main() {
-    int a, b, x;
+    int a, b, c;
 
-    std::cin >> a >> b >> x;
+    std::cin >> a >> b >> c;
 
-    if (x < a) {
-        std::cout << "1" << std::endl;
-    } else if (x == a) {
-        std::cout << "2" << std::endl;
-    } else if (a < x && x < b) {
-        std::cout << "3" << std::endl;
-    } else if (x == b) {
-        std::cout << "4" << std::endl;
-    } else if (x > b) {
-        std::cout << "5" << std::endl;
+    if (a > b && a > c) {
+        // a is biggest
+        if (b > c) {
+            // b is middle
+            // c is lowest
+            std::cout << a << ' ' << b << ' ' << c << std::endl;
+        } else {
+            // c is middle
+            // b is lowest
+            std::cout << a << ' ' << c << ' ' << b << std::endl;
+        }
+    } else if (b > a && b > c) {
+        // b is biggest
+        if (a > c) {
+            // a is middle
+            // c is lowest
+            std::cout << b << ' ' << a << ' ' << c << std::endl;
+        } else {
+            // c is middle
+            // a is lowest
+            std::cout << b << ' ' << c << ' ' << a << std::endl;
+        }
+    } else {
+        // c is biggest
+        if (a > b) {
+            // a is middle
+            // b is lowest
+            std::cout << c << ' ' << a << ' ' << b << std::endl;
+        } else {
+            // b is middle
+            // a is lowest
+            std::cout << c << ' ' << b << ' ' << a << std::endl;
+        }
     }
 
     return 0;
--- Solutions/Operators And Ranges/task03.cpp
+++ Solutions/Operators And Ranges/task03.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 int main() {
-    int a, b, c, x;
+    int a, b, x;
 
-    std::cin >> a >> b >> c;
+    std::cin >> a >> b >> x;
 
-    do {
-        std::cin >> x;
-    } while(!(a < x && x < b) || (x > c));
+    if (x < a) {
+        std::cout << "1" << std::endl;
+    } else if (x == a) {
+        std::cout << "2" << std::endl;
+    } else if (a < x && x < b) {
+        std::cout << "3" << std::endl;
+    } else if (x == b) {
+        std::cout << "4" << std::endl;
+    } else if (x > b) {
+        std::cout << "5" << std::endl;
+    }
 
     return 0;
 }
