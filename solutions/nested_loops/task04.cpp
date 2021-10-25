9b1ce283d2e4d792f3603749cefab043c7e8ac13
SinestroWhite <g.yokov@hotmail.com>
2021-10-25 13:41:49 +0300

    Add solutions to nested loops

---
+++ solutions/nested_loops/task04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i; j <= n; ++j) {
+            for (int k = j; k <= n; ++k) {
+                if (i*i + j*j == k*k || i*i + k*k == j*j || k*k + j*j == i*i) {
+                    cout << i << " " << j << " " << k << endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ solutions/nested_loops/task06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 100; i <= 999; ++i) {
+        if (i % 10 + (i / 10) % 10 + i / 100 == n) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
