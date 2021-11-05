d51a0101d8cc7c4c353fc3562c51fe7780c62c43
Martin Iliev <mapmoeu@gmail.com>
2021-11-05 19:04:39 +0200

    Add solution to task 5 from arrays

---
+++ solutions/arrays/task05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+#define size 7
+int main() {
+	int arr[size] = {1, 5, 7, 12, 5, 1, 2};
+
+	int max_ind = 0;
+
+	for (int i = 1; i < size; i++) {
+		if (arr[max_ind] < arr[i]) {
+			max_ind = i;
+		}
+	}
+
+	std::cout << "Max element arr[" << max_ind << "] is " << arr[max_ind] << "\n";
+
+	return 0;
+}
