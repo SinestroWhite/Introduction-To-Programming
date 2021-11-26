fd201836bbe41535019437e2fad4747850b33ecf
Martin Iliev <mapmoeu@gmail.com>
2021-11-26 16:08:22 +0200

    Add solutions to pointers_and_references tasks

---
+++ solutions/pointers_and_references/task06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int arr[] = {1, 2, 3, 7, 8};
+	// addresses
+	for (int i = 0; i < 5; i++) {
+		std::cout << (arr + i) << ' ';
+	}
+	std::cout << '\n';
+
+	// values
+	for (int i = 0; i < 5; i++) {
+		std::cout << *(arr + i) << '\t';
+	}
+	std::cout << '\n';
+	return 0;
+}
