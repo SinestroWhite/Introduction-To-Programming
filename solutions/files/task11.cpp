e9b648c747c6ff108962eaafa081b1f97322d42d
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-12-03 11:14:02 +0200

    Add more solutions for Week08 tasks

---
+++ solutions/files/task11.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string filePath;
+
+    std::cin >> filePath;
+
+    std::ifstream file(filePath);
+
+    if(!file.is_open()) {
+        std::cout << "ERROR OPENING FILE\n";
+        return 1;
+    }
+
+    bool areSet = false;
+    size_t smallestRowIndex, smallestRowSize,
+           biggestRowIndex, biggestRowSize;
+
+    std::string buffer;
+
+    for (int i = 1; std::getline(file, buffer); ++i) {
+        if (!areSet) {
+            smallestRowIndex = i;
+            smallestRowSize = buffer.size();
+            biggestRowIndex = i;
+            biggestRowSize = buffer.size();
+            areSet = true;
+        }
+
+        if (buffer.size() < smallestRowSize) {
+            smallestRowIndex = i;
+            smallestRowSize = buffer.size();
+        }
+
+        if (buffer.size() > biggestRowSize) {
+            biggestRowIndex = i;
+            biggestRowSize = buffer.size();
+        }
+    }
+
+    if (areSet) {
+        std::cout << "Smallest row index/size: " << smallestRowIndex << '/' << smallestRowSize << '\n';
+        std::cout << "Biggest row index/size: " << biggestRowIndex << '/' << biggestRowSize << '\n';
+    }
+
+    file.close();
+}
---
+++ solutions/strings/task14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str;
+
+    std::getline(std::cin, str);
+
+    size_t leftIndex = 0, rightIndex = 0;
+
+    for (int index = 0; index < str.size(); ++index) {
+        for (int reverseIndex = str.size() - 1; reverseIndex >= index; --reverseIndex) {
+            if (str[index] == str[reverseIndex]) {
+                if ((reverseIndex - index) > (rightIndex - leftIndex)) {
+                    leftIndex = index;
+                    rightIndex = reverseIndex;
+                }
+                break;
+            }
+        }
+    }
+
+    std::cout << leftIndex << ' ' << rightIndex << '\n';
+
+    return 0;
+}
